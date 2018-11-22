2f2b8928fc67fcbe0cf3846d2731158436511158
stardiviner <numbchild@gmail.com>
2018-11-23 07:56:29 +0800

    define old/new website URL as variable, and get the URL's HTML.

--- src/website_compare/website_new.clj
+++ src/website_compare/website_new.clj
@@ -2,5 +2,30 @@
 ;; "http://zjzx.sxsedu.net"
 
 (ns website-compare.website-new
+  (:require [clj-http.client :as http]
+            [net.cgrand.enlive-html :as html])
+  (:import java.net.URL))
+
+(defonce website-new-url "http://www.sxszjzx.com")
+
+;; (defn get-html
+;;   "Get HTML string as result."
+;;   [url]
+;;   ;; faster, use tagsoup internal. But I don't know how to specify encoding.
+;;   (try
+;;     (html/html-resource (URL. url))
+;;     (catch Exception e
+;;       (println (format "Error URL: %s" url))
+;;       (println (format "URL error: Unknown Host \n %s" e)))))
+
+(defn get-html
+  "Get HTML string as result."
+  [url]
+  (-> (http/get url {:as "UTF8"})
+      :body
+      html/html-snippet))
+
+(defonce website-new-html (get-html website-new-url))
+
   )
 
--- src/website_compare/website_old.clj
+++ src/website_compare/website_old.clj
@@ -1,4 +1,29 @@
 ;;; http://www.sxti.zj.cn
 
 (ns website-compare.website-old
-  )
+  (:require [clj-http.client :as http]
+            [net.cgrand.enlive-html :as html])
+  (:import java.net.URL)
+  (:require [ring.util.codec :only [form-decode]])
+  (:require [clojure.walk :only [keywordize-keys]]))
+
+(defonce website-old-url "http://www.sxti.zj.cn")
+
+;;; Selectors
+(def selector-nav [:div.head_3 :ul#head_nav])
+(def selector-content [:div.page_1])
+(def selector-sidebar [:div.page_left :div.pleft_t3])
+(def selector-article [:div.page_right :div.pright_t3])
+
+(defn get-html
+  "Get HTML string as result."
+  [url]
+  (try
+    (-> (http/get url {:as "GB2312"})
+        :body
+        html/html-snippet)
+    (catch java.net.UnknownHostException e
+      (println (format "[get-html] Error URL: %s" url))
+      (println (format "[get-html] %s" e)))))
+
+(defonce website-old-html (get-html "http://www.sxti.zj.cn/"))
