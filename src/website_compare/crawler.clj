3c58d5eb8322fcf9181fe42838e69e04d1445298
stardiviner <numbchild@gmail.com>
2018-11-23 08:04:57 +0800

    [crawler] crawl old/new website all article links

--- src/website_compare/crawler.clj
+++ src/website_compare/crawler.clj
@@ -1,5 +1,11 @@
 (ns website-compare.crawler
+  (:require [clj-http.client :as http]
+            [net.cgrand.enlive-html :as html])
+  (:import java.net.URL)
   (:require [taoensso.carmine :as redis])
+  (:require [website-compare.website-old :as old])
+  (:require [website-compare.website-new :as new])
+  )
 
 ;;; Redis store crawler links
 (defonce redis-conn-pool {:pool {}
@@ -13,4 +19,38 @@
 (comment
   (wcar*
    (redis/ping)))
+
+;;;crawl links
+
+(defn crawl-website-old-links []
+  "Crawl all links of website old."
+  (for [nav-link (into #{}            ; use set to remove duplicate links.
+                       (map #(-> % vals first) old/nav-bar-links-map))]
+    (for [article-link (old/get-all-article-links nav-link)]
+      (wcar*
+       ;; clean up all links at firts.
+       (redis/del :old/links)
+       (redis/lpush :old/links article-link)))))
+
+(comment
+  (crawl-website-old-links))
+
+(comment
+  (wcar* (redis/hvals :old/links)))
+
+(defn crawl-website-new-links []
+  "Crawl all links of website new."
+  ;; clean up all links at firts.
+  (wcar* (redis/del :new/links))
+  (for [nav-link (into #{}            ; use set to remove duplicate links.
+                       (map #(-> % vals first) new/nav-bar-links-map))]
+    (for [article-links-in-a-page (new/get-all-article-links nav-link)]
+      (for [article-link article-links-in-a-page]
+        (wcar* (redis/lpush :new/links (first (vals article-link))))))))
+
+(comment
+  (crawl-website-new-links))
+
+(comment
+  (wcar* (redis/get :new/links)))
 
