4855d7476896d39b429051f5ddecc44d3199fcde
stardiviner <numbchild@gmail.com>
2018-11-23 08:07:55 +0800

    [storage] use JDBC to save to SQLite

--- src/website_compare/store.clj
+++ src/website_compare/store.clj
@@ -1,3 +1,14 @@
 (ns website-compare.store
+  (:require [clojure.java.jdbc :as jdbc])
+  (:require [website-compare.crawler :as crawler]))
+
+(def sqlite
+  {:dbtype "sqlite"
+   :dbname "db.sqlite"}
+  ;; {:classname   "org.sqlite.JDBC"
+  ;;  :subprotocol "sqlite"
+  ;;  :subname     "db.sqlite"}
   )
 
+(comment
+  (jdbc/query sqlite "SELECT 1+1 as result"))
