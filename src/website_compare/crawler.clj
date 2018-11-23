f1a8a434b943cde2c1188f6a16ee11e637a24141
stardiviner <numbchild@gmail.com>
2018-11-23 08:02:06 +0800

    [crawler] use Redis as links storage

--- src/website_compare/crawler.clj
+++ src/website_compare/crawler.clj
@@ -1,3 +1,16 @@
 (ns website-compare.crawler
-  )
+  (:require [taoensso.carmine :as redis])
+
+;;; Redis store crawler links
+(defonce redis-conn-pool {:pool {}
+                          :spec {:host "127.0.0.1" :port 6379}})
+
+;;; define a macro for binding Redis connection pool as default environment.
+(defmacro wcar*
+  [& body]
+  `(redis/wcar redis-conn-pool ~@body))
+
+(comment
+  (wcar*
+   (redis/ping)))
 
